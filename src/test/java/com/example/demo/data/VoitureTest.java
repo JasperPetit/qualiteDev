dfc9861a548517fe12857287741dd3748e2c1bf8
Jasper <jasper.petit27@gmail.com>
2026-04-20 15:25:30 +0200

    Fonction test voiture

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -6,12 +6,48 @@ import org.springframework.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
+
 @SpringBootTest
 public class VoitureTest {
 
-    @Test
+    private Voiture v1;
+    @BeforeEach
     void creerVoiture(){
-        assertEquals(1,1);
+        v1 = new Voiture();
     }
+
+    @Test
+    void modifierId(){
+        v1.setId(123);
+
+        int testId = v1.getId();
+        assertEquals(testId,123);
+    }
+
+    @Test
+    void modifierMarque(){
+        v1.setMarque("Mercedes");
+
+        String testMarque = v1.getMarque();
+        assertEquals(testMarque, "Mercedes");
+    }
+
+    @Test
+    void modifierPrix(){
+        v1.setPrix(1000);
+
+        int prixTest = v1.getPrix();
+        assertEquals(prixTest,1000);
+    }
+
+    @Test
+    void testToString(){
+        Voiture v2 = new Voiture("Audi",2000);
+        v2.setId(9);
+        String expectedString = "Car{marque='Audi', prix=2000, id=9}";
+        assertEquals(v2.toString(),expectedString );
+    }
+
 
 }
